0aea485bea9651d97ac18654429b92800b3bf102
evandersloot <edvandersloot@gmail.com>
2021-08-23 15:07:40 -0500

    added styling to login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
+
+import './login-view.scss';
+import logo from '../img/logo.png';
 
 export function LoginView(props) {
     const [ username, setUsername ] = useState('');
@@ -12,17 +20,29 @@ export function LoginView(props) {
     };
 
     return (
-        <form>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
-        </form>
+        <Col md={8} className="mx-auto">
+            <Row className="d-flex mt-2 justify-content-center">
+                <Image className="w-75 h-25 m-auto" src={logo} />
+                <h3 className="text-center">Sign in with your Username and Password</h3>
+            </Row>
+            <Row>
+                <Form className="d-flex h-75 w-100 justify-content-center">
+                    <Col md={12} className="w-100 mt-2 mb-10">
+                        <Form.Group className="mb-3" controlId="formUserName">
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control type="text" placeholder="Choose an alphanumeric Username" value={username} onChange={ e => setUsername(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPassword">
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control type="text" placeholder="Set a Password" value={password} onChange={ e => setPassword(e.target.value)} />
+                        </Form.Group>
+                        <div className="login-button pb-5 mb-5">
+                        <Button type="submit" value="Submit" onClick={handleSubmit}>Log In</Button>
+                        </div>
+                    </Col>
+                </Form>
+            </Row>
+        </Col>
     );
 }
 
