dd78d4eb8d1cb9cc90ff5f333b3674ba448623d5
evandersloot <edvandersloot@gmail.com>
2021-08-23 15:08:24 -0500

    added styling to movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
 
 import './movie-card.scss';
 
@@ -10,14 +11,18 @@ export class MovieCard extends React.Component {
         const { movie, onMovieClick } = this.props;
                 
         return (
-            <Card>
+            <Card className="h-100 mt-2 pt-1">
                 <Card.Img variant='top' src={movie.ImagePath} />
                 <Card.Body>
                     <Card.Title>{movie.Title}</Card.Title>
                     <Card.Text>{movie.Description}</Card.Text>
-                    <Button onClick={() => onMovieClick(movie)} variant='link'> Open</Button>
-                </Card.Body>
-                </Card>                 
+                </Card.Body>    
+                <Card.Footer>
+                    <div className="open-button">
+                    <Button onClick={() => onMovieClick(movie)}>Open</Button>
+                    </div>
+                </Card.Footer>
+            </Card>                 
         );
     }
 }
