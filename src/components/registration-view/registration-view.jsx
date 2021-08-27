b51868f86df899c905a5297de3ee86703f9e9d58
evandersloot <edvandersloot@gmail.com>
2021-08-26 21:09:02 -0500

    updated registration-view

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
@@ -16,12 +18,67 @@ export function RegistrationView(props) {
     const [ email, setEmail ] = useState('');
     const [ birthday, setBirthday ] = useState('');
 
+    const [ nameError, setNameError ] = useState({});
+    const [ usernameError, setUserNameError ] = useState({});
+    const [ passwordError, setPasswordError ] = useState({});
+    const [ emailError, setEmailError ] = useState({});
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(name, username, password, email, birthday);
-        props.onSignUp(username);
+        const isValid = formValidation();
+        if (isValid) {
+            axios.post('https://flixspotter.herokuapp.com/users', {
+                name: name,
+                username: username,
+                password: password,
+                email: email,
+                birthday: birthday
+            })
+            .then(response => {
+                const data = response.data;
+                console.log(data);
+                window.open('/', '_self');
+            })
+            .catch(e => {
+                console.log('an error has occurred')
+            });
+        }
     };
+
+    const formValidation = () => {
+        const nameError = {};
+        const usernamError = {};
+        const passwordError = {};
+        const emailError = {};
+        let isValid = true;
+
+        if (name.length < 2 || name === '') {
+            nameError.nameNeeded = 'Please input your name.';
+            isValid = false;
+        }
+        
+        if (username.length < 5 || username === '') {
+            usernameError.UsernameTooShort = 'Username must be at least 5 alphanumeric characters.';
+            isValid = false;
+        }
+
+        if (password.length < 8 || password === '') {
+            passwordError.passwordTooShort = 'Password must be at least 8 characters.';
+            isValid = false;
+        }
+
+        if (!email || email.indexOf('@') === -1) {
+            emailError.notValidEmail = 'Your email is not valid.';
+            isValid = false;
+        };
+
+        setNameError(nameError);
+        setUserNameError(usernameError);
+        setPasswordError(passwordError);
+        setEmailError(emailError);
+        return isValid;
+    }
 
     return (
         <Col md={8} className="mx-auto">
@@ -30,27 +87,55 @@ export function RegistrationView(props) {
                     <h3 className="text-center">Please fill in all fields to sign up!</h3>
             </Row>
            
-                <Row ClassName="d-flex">
+                <Row className="d-flex">
                     <Form className="d-flex h-75 w-100 justify-content-center">
                         <Col md={12} className="w-100 mt-2 mb-10">
                             <Form.Group className="mb-3" controlId="formName">
                                 <Form.Label>Name:</Form.Label>
                                 <Form.Control type="text" placeholder="First and Last Name" value={name} onChange={ e => setName(e.target.value)} />
+                                {Object.keys(nameError).map((key) => {
+                                    return (
+                                        <div key={key}>
+                                            {nameError[key]}
+                                        </div>
+                                    );
+                                })}
                             </Form.Group>
                         
                             <Form.Group className="mb-3" controlId="formUserName">
                                 <Form.Label>Username:</Form.Label>
                                 <Form.Control type="text" placeholder="Choose an alphanumeric Username" value={username} onChange={ e => setUsername(e.target.value)} />
+                                {Object.keys(usernameError).map((key) => {
+                                    return (
+                                        <div key={key}>
+                                            {usernameError[key]}
+                                        </div>
+                                    );
+                                })}
                             </Form.Group>
                             
                             <Form.Group className="mb-3" controlId="formPassword">
                                 <Form.Label>Password:</Form.Label>
                                 <Form.Control type="text" placeholder="Set a Password" value={password} onChange={ e => setPassword(e.target.value)} />
+                                {Object.keys(passwordError).map((key) => {
+                                    return (
+                                        <div key={key}>
+                                            {passwordError[key]}
+                                        </div>
+                                    );
+                                })}
                             </Form.Group>
                             
                             <Form.Group className="mb-3" controlId="formEmail">
                                 <Form.Label>Email Address:</Form.Label>
                                 <Form.Control type="email" placeholder="name@example.com" value={email} onChange={ e => setEmail(e.target.value)} />
+                                {Object.keys(emailError).map((key) => {
+                                    return (
+                                        <div key={key}>
+                                            {emailError[key]}
+                                        </div>
+                                    );
+                                })}
                             </Form.Group>
                             
                             <Form.Group className="mb-3" controlId="formBirthday">
@@ -76,5 +161,5 @@ RegistrationView.propTypes = {
         email: PropTypes.string.isRequired,
         birthday: PropTypes.string.isRequired
     }),
-    onSignUp: PropTypes.func.isRequired
+    onSignUp: PropTypes.func,
 };
